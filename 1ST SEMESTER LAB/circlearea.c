66d4c9ab62622815f883f1eb1e1fa769bec4ebd5
Md. Mehedi Hasan <106642643+0mehedihasan@users.noreply.github.com>
2022-07-22 14:01:59 +0600

    Add files via upload

---
+++ 1ST SEMESTER LAB/circlearea.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+  float radius,area;
+printf("Enter the Vale of Radius");
+scanf("%f",&radius);
+area=3.1416*radius*radius;
+printf("The area is=%f\n",area);
+    return 0;
+}
---
+++ 1ST SEMESTER LAB/cse-26_01_22.c
@@ -0,0 +1,22 @@
+
+#include<stdio.h>
+int main()
+{
+    
+int x;
+float y;
+char z;
+double a;
+
+/* size fo variable */
+
+printf("size of int: %zu bytes\n", sizeof(x));
+printf("size of float: %zu bytes\n", sizeof(y));
+printf("size of char: %zu bytes\n", sizeof(z));
+printf("size of double: %zu bytes\n", sizeof(a));
+
+return 0;
+
+}
+
+                
